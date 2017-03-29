cc1bb5c31c361882df390a1668cd8e0943e7730a
ehmed-kino <ehmedaff@gmail.com>
2017-03-29 03:18:14 +0100

    handle the post request from UI

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// allow to Access this server (CORS permissions) :)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// this the middleware that handle the body of the request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', require('./routes/routes'));
 
