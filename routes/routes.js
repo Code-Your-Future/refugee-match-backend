9a16b48a6718b939646c8477f928da8c286503f9
ehmed-kino <ehmedaff@gmail.com>
2017-03-30 01:16:34 +0100

    read the data in the get method

--- routes/routes.js
+++ routes/routes.js
@@ -4,27 +4,29 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-let data = {};
-
 // get the list from the db
 router.get('/', function(req, res) {
-  res.send(data);
+
+  fs.readFile('./data/data.json', 'utf-8', (err, data) => {
+    if (err) throw err
+    return res.send(data);
+  })
 });
 
 // post the list to the db
 router.post('/', function(req, res) {
+  res.send({type:'POST'});
+  console.log("this is the data", req.body);
+
   // save it in data.json
   fs.readFile('./data/data.json', 'utf-8', function(err, data) {
   	if (err) throw err
     // we can handle the data here to add new one with the previouse data
-  	fs.writeFile('./data/data.json', JSON.stringify(req.body), 'utf-8', function(err) {
+  	fs.writeFile('./data/data.json', JSON.stringify(req.body), function(err) {
   		if (err) throw err
   	})
   })
-  data = req.body;
-  console.log(req.body);
-  res.send({type:'POST'});
-});
+})
 
 // update the list in the db
 router.put('/:id', function(req, res) {
